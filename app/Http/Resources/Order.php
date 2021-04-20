868a2e0862f3133eee4b5d46755fec98b40fdc72
DUSSARTLuke <lukedussart@hotmail.fr>
2021-04-20 12:21:53 +0200

    ajout date création commande

--- app/Http/Resources/Order.php
+++ app/Http/Resources/Order.php
@@ -16,12 +16,21 @@ class Order extends JsonResource
     public function toArray($request)
     {
 
+        $moisFr = [
+            'janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre',
+            'novembre', 'décembre'
+        ];
+
+        list($jour, $mois, $annee) = explode('/', date("d/n/Y", strtotime($this->created_at)));
+
         $data =  [
+            'id' => $this->id,
             'numOrder' => $this->numOrder,
             'product' => DB::table('order_products')->where(['order_id' => $this->id])->get(['product_id', 'quantite']),
             'price' => $this->price,
             'quantites' => 0,
-            'produits' => []
+            'produits' => [],
+            'date_commande' => $jour . ' ' . $moisFr[$mois] . ' ' . $annee,
         ];
 
         foreach ($data['product'] as $prod) {
